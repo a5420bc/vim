54486fb618e8ed8a4f223b8075c6a58d0cdbdd6e
bingjie6 <bingjie6@staff.weibo.com>
2020-10-28 17:19:03 +0800

    修改安装脚本

--- .vim_runtime/install_awesome_vimrc.sh
+++ .vim_runtime/install_awesome_vimrc.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
+cd ~/vim/.vim_runtime
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/vim/.vim_runtime
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/vim/.vim_runtime/vimrcs/basic.vim
+source ~/vim/.vim_runtime/vimrcs/filetypes.vim
+source ~/vim/.vim_runtime/vimrcs/plugins_config.vim
+source ~/vim/.vim_runtime/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/vim/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
